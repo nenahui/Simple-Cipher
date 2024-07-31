2eb626bd15f1942bd7c2264ae6893b05977ec440
nenahui <kanatraccoon@gmail.com>
2024-08-01 03:25:30 +0600

    Completed 2 task

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
 const app = express();
 const port = 8000;
+const password = 'password';
 
 app.get('/:url', (req, res) => {
 	return res.send(req.params.url);
+});
+
+app.get('/encode/:text', ({params}, res) => {
+  const encoded = Vigenere.Cipher(password).crypt(params.text);
+  return res.send(encoded);
+});
+
+app.get('/decode/:text', ({params}, res) => {
+  const decoded = Vigenere.Decipher(password).crypt(params.text);
+  return res.send(decoded);
 });
 
 app.listen(port, () => {
